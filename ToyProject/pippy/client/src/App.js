5387fe55ac55c21509fe6cd51ed108346b124333
choigicheol <kofpo900916@gmail.com>
2021-09-14 16:49:10 +0900

    [MainPage][Login] 메인페이지 기능 병합

---
+++ ToyProject/pippy/client/src/App.js
@@ -0,0 +1,16 @@
+import LoginForm from './pages/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+      {/* <SignUp /> */}
+      {/* <ForgotPassworModal /> */}
+      {/* <LogoutModal /> */}
+      {/* <DeleteTweet /> */}
+      {/* <SideBar /> */}
+    </div>
+  );
+}
+
+export default App;
